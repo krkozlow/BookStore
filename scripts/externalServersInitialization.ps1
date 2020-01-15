3d878e6e211230f78c017a954f7de10cd6fc1db8
Krzysztof Kozlowski <krzysztof.kozlowski@pl.whoohoo.com>
2020-01-15 13:38:56 +0100

    test page

--- scripts/externalServersInitialization.ps1
+++ scripts/externalServersInitialization.ps1
@@ -1,2 +1,3 @@
+docker run --name rabbitmq -d -p 5672:5672 -p 5673:5673 -p 15672:15672 --net=host --hostname rabbitmq rabbitmq:3-management
 docker run -d -p 5672:5672 --net=host rabbitmq
 docker run -d -p 27017-27019:27017-27019 --net=host mongo
